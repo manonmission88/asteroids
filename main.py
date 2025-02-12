ff08c1831ad0c248fb08a5e7cfbd0e7f836e7ec8
manonmission88 <manishnewray88@gmail.com>
2025-02-12 13:40:50 -0500

    roate left right feature

--- main.py
+++ main.py
@@ -7,19 +7,21 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.time.Clock()
+    clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
         
     
-    dt = clock.tick(60)
+    
 
 if __name__ == "__main__":
     main()
