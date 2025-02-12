05b96fb6568cdf7c1d1244641e4652a29408e750
manonmission88 <manishnewray88@gmail.com>
2025-02-12 13:26:08 -0500

    Making triangle shape

--- main.py
+++ main.py
@@ -3,18 +3,21 @@
 # throughout this file
 import pygame
 from constants import *
-
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
     
     dt = clock.tick(60)
 
